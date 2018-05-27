c3e698d07f61d0c3772e3f331c30bf0dc32c82df
garwahl <garwahlam@gmail.com>
2018-05-27 18:26:37 +1000

    Progress on extraction

--- alpr.py
+++ alpr.py
@@ -4,9 +4,10 @@
 # Email: hello@garwahlam.com         #
 ######################################
 
-import numpy as np
-import cv2
 import argparse
+import cv2
+import mahotas
+import numpy as np
 
 # Setup
 ap = argparse.ArgumentParser()
@@ -15,14 +16,32 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 
 median_kernal  = 5
-average_kernal = 20 
+average_kernal = (20,20) 
+x_deriv = 1
+y_deriv = 0
 
-# License Plate Extraction
+############################
+# License Plate Extraction #
+############################
+
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-image = cv2.medianBlur(image, median_kernal)
-image = cv2.blur(image, average_kernal)
+blurred = cv2.medianBlur(image.copy(), median_kernal)
+blurred = cv2.blur(blurred, average_kernal)
+intensity_diff = image - blurred
 
-# License Plate Segmentation
+# Binarization with Otsu's method
+threshold = mahotas.thresholding.otsu(intensity_diff)
+# Sobel Filter
+cv2.imshow("Original",image)
+cv2.imshow("Blurred", blurred)
+cv2.imshow("Thresholded", thresh)
 
+cv2.waitKey(0)
 
-# Character recognition
+##############################
+# License Plate Segmentation #
+##############################
+
+#########################
+# Character recognition #
+#########################
